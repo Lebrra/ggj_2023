3036accb6b9b9aab75c0975dd9d65672ade33f21
Leah Blasczyk <scout1665@gmail.com>
2023-02-02 20:26:06 -0600

    Tree stuff. Edited gitignore

---
+++ rootsProject/Assets/Scripts/Trees/TreeChunkLogic.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeChunkLogic : MonoBehaviour
+{
+    [SerializeField]
+    Transform startPoint;
+    [SerializeField]
+    Transform endPoint;
+}
